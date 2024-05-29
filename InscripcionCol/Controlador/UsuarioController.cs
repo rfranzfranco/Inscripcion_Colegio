a495b1663129e413f2d92b21ae3534d56c545102
Elian0 <>
2024-05-29 04:43:08 -0400

    Creacion de Botones de guardar, UusarioController, UsuarioView

---
+++ InscripcionCol/Controlador/UsuarioController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Validation;
+using System.Windows.Forms;
+using InscripcionCol.Modelo;
+
+namespace InscripcionCol.Controlador
+{
+    internal class UsuarioController
+    {
+        private readonly RegistroColegioDBEntities _db = new RegistroColegioDBEntities();
+
+        public List<UsuarioViewModel> Listar()
+        {
+            try
+            {
+                var query = from usuario in _db.TUsuario
+                            join registro in _db.TRegistro on usuario.id_registro equals registro.id_registro
+                            select new UsuarioViewModel
+                            {
+                                ci = registro.ci,
+                                nombre = registro.nombre,
+                                ap_paterno = registro.ap_paterno,
+                                ap_materno = registro.ap_materno,
+                                direccion = registro.direccion,
+                                celular = registro.celular,
+                                sexo = registro.sexo,
+                                fecha_nac = registro.fecha_nac,
+                                usuario = usuario.usuario,
+                                contraseña = usuario.contraseña,
+                                rol = usuario.rol
+                            };
+
+                return query.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ha ocurrido un error al listar los usuarios: {ex.Message}");
+                return new List<UsuarioViewModel>();
+            }
+        }
+
+        public async Task<bool> RegistrarUsuarioAsync(TRegistro registro, TUsuario usuario)
+        {
+            using (var transaction = _db.Database.BeginTransaction())
+            {
+                try
+                {
+                    _db.TRegistro.Add(registro);
+                    await _db.SaveChangesAsync();
+
+                    usuario.id_registro = registro.id_registro;
+                    _db.TUsuario.Add(usuario);
+                    await _db.SaveChangesAsync();
+
+                    transaction.Commit();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }      
+
+    }
+}
+
---
+++ InscripcionCol/Controlador/UsuarioViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InscripcionCol.Controlador
+{
+    internal class UsuarioViewModel
+    {
+        public int ci { get; set; }
+        public string nombre { get; set; }
+        public string ap_paterno { get; set; }
+        public string ap_materno { get; set; }
+        public string direccion { get; set; }
+        public int celular { get; set; }
+        public string sexo { get; set; }
+        public System.DateTime fecha_nac { get; set; }
+        public string usuario { get; set; }
+        public string contraseña { get; set; }
+        public string rol { get; set; }
+    }
+}
